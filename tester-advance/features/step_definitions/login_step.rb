7f165007cd7371491c79d74c49ff250fab7544d7
FaustDuong <69310760+FaustDuong@users.noreply.github.com>
2020-09-30 22:54:07 +0700

    Add files via upload

---
+++ tester-advance/features/step_definitions/login_step.rb
@@ -0,0 +1,38 @@
+Given('navigate to login page') do
+    @driver = Selenium::WebDriver.for :firefox
+    @driver.get 'http://the-internet.herokuapp.com/login'
+    @driver.manage.window.maximize
+end
+
+When('user type {string} and {string}') do |string, string2|
+    @driver.find_element(id: 'username').send_keys string
+    @driver.find_element(id: 'password').send_keys string2
+    #sleep 2
+    @driver.find_element(css: ".fa.fa-2x.fa-sign-in").click
+end
+  
+Then('the msg {string} displays') do |string|
+    @msg = string
+    expect(@driver.find_element(id: 'flash').text).to include(@msg)
+    @driver.quit
+end
+
+When ('I type invalid username {string}') do |string|
+    @username = string
+    @driver.find_element(id: 'username').send_keys(@username)
+end
+
+When ('I type invalid password {string}') do |string|
+    @password = string
+    @driver.find_element(id: 'password').send_keys(@password)
+end
+
+When('press Login') do
+    @driver.find_element(xpath: "//i[contains(text(),'Login')]").click
+end
+
+Then ('{string} displays') do |string|
+    @msg1 = string
+    expect(@driver.find_element(id: 'flash').text).to include(@msg1)
+    @driver.quit
+end
